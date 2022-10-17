00a044c428d980996ce8f8879ea58e45cb3fad10
Vadim Shershen <vadim.shershen.97@mail.ru>
2022-10-18 01:23:24 +0300

    feat: complite queue.js

--- src/queue.js
+++ src/queue.js
@@ -13,8 +13,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
+class Queue {
+  constructor() {
+    this.head = null;
+    this.body = null;
+  }
+
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    let newNode = new ListNode(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.body = newNode;
+    } else {
+      this.body.next = newNode;
+      this.body = newNode;
+    }
+  }
+
+  dequeue() {
+    let data = this.head.value;
+    this.head = this.head.next;
+    return data;
+  }
 }
 
 module.exports = {
